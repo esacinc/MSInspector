d74b7e3a30394e9fd4b095bdf24cc0cca13f966f
yin lu <yin.lu@esacinc.com>
2024-02-09 16:09:30 -0500

    Update Python from 2.7 to 3.11

--- src/MSInspector/utils/fileProcess.py
+++ src/MSInspector/utils/fileProcess.py
@@ -26,7 +26,7 @@ def judgeInteger(df_test, columnName):
         integerStatus = True
     return integerStatus
 
-def headerModify(inf_tmp, outf_tmp, experiment_type_tmp, skyzip_file_dir_baseName, fileNameInferredExpStatusDic, fileNameEmptyStatusDic):
+def headerModify_old(inf_tmp, outf_tmp, experiment_type_tmp, skyzip_file_dir_baseName, fileNameInferredExpStatusDic, fileNameEmptyStatusDic):
     # This function is to remove space in the column name at the first row of inf_tmp.
     outf = open(outf_tmp, 'w')
     with open(inf_tmp, 'r') as inf:
@@ -100,3 +100,69 @@ def headerModify(inf_tmp, outf_tmp, experiment_type_tmp, skyzip_file_dir_baseNam
     #else:
     #    skylineTemp_type = None
     return skylineTemp_type
+
+def headerModify(inf_tmp, outf_tmp, experiment_type_tmp, skyzip_file_dir_baseName, fileNameInferredExpStatusDic, fileNameEmptyStatusDic):
+	# This function is to remove space in the column name at the first row of inf_tmp.
+	outf = open(outf_tmp, 'w')
+	with open(inf_tmp, 'r') as inf:
+		line = inf.readline()
+		outf.write(line.replace(' ', ''))
+		line = inf.readline()
+		while line != '':
+			# replace #N/A with '', so that when R read this file, it will transform '' into NA automatically.
+			outf.write(line.replace('#N/A', ''))
+			line = inf.readline()
+	outf.close()
+	#os.remove(inf_tmp)
+	# Inferred experiment type from outf_tmp.
+	# Read outf_tmp into a dataframe and keep all the values in the format of string.
+	if os.path.splitext(outf_tmp)[1].lower() == '.tsv':
+		sepType = '\t'
+	else:
+		sepType = ','
+	df_tmp = pd.read_csv(outf_tmp, sep=sepType, header=0, converters={i: str for i in range(0, 100)})
+	if df_tmp.shape[0] == 0:
+		fileNameEmptyStatusDic.update({skyzip_file_dir_baseName:True})
+		fileNameInferredExpStatusDic.update({skyzip_file_dir_baseName:None})
+		skylineTemp_type = None
+	else:
+		fileNameEmptyStatusDic.update({skyzip_file_dir_baseName:False})
+		# Start to infer the experiment type. 
+		#exp3Stutus = judgeColumnNonblank(df_tmp, 'Exp3SampleGroup')
+		exp4Stutus = judgeColumnNonblank(df_tmp, 'Exp4SampleGroup')
+		exp2NewTemplateSatuts = judgeColumnNonblank(df_tmp, 'Exp2SampleGroup')
+		dayStatus = judgeColumnNonblank(df_tmp, 'Day')
+		analyteConcentrationStatus = judgeColumnNonblank(df_tmp, 'AnalyteConcentration')
+		internalStandardConcentrationStatus = judgeColumnNonblank(df_tmp, 'InternalStandardConcentration')
+		if exp4Stutus:
+			inferredExpType = 'exp4'
+			skylineTemp_type = None
+		elif exp2NewTemplateSatuts:
+			inferredExpType = 'exp2'
+			skylineTemp_type = 'new'
+		elif (not exp2NewTemplateSatuts) and (not exp4Stutus) and dayStatus:
+			inferredExpType = 'exp5'
+			skylineTemp_type = None
+		elif (not exp2NewTemplateSatuts) and (not exp4Stutus) and analyteConcentrationStatus:
+			if internalStandardConcentrationStatus:
+				inferredExpType = 'exp1'
+				skylineTemp_type = 'new'
+			else:
+				inferredExpType = 'exp3'
+				skylineTemp_type = None
+		else:
+			# The skyline file must be from exp1 and exp2 of old templates which have the same columns names.
+			# Infer the experiment type base the content of column SampleGroup and Concentration.
+			sampleGroupStatus = judgeSampleGroupInExp2(df_tmp)
+			integerStatus = judgeInteger(df_tmp, 'Concentration')
+			if sampleGroupStatus and integerStatus:
+				inferredExpType = 'exp2'
+				skylineTemp_type = 'old'
+			else:
+				inferredExpType = 'exp1'
+				skylineTemp_type = 'old'
+		if inferredExpType != experiment_type_tmp:
+			fileNameInferredExpStatusDic.update({skyzip_file_dir_baseName:True})
+		else:
+			fileNameInferredExpStatusDic.update({skyzip_file_dir_baseName:False})
+	return skylineTemp_type
